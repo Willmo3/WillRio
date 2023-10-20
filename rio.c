c1c60e75dd5cc2b8d53b999458b93a8a9665c02d
Willmo3 <thelordofbouge@gmail.com>
2023-10-20 11:38:54 -0400

    Implement nwritten

--- rio.c
+++ rio.c
@@ -7,11 +7,11 @@ ssize_t rio_readn(int fd, void *usrbuf, size_t n) {
 
   while (nleft > 0) {
     if ((nread = read(fd, bufp, nleft)) < 0) {
-      // Check for interrupt by sig handler return.
+      // Check for interrupt by extraneous sig handler return.
       if (errno == EINTR)
         nread = 0;
       else
-        return -1; // Unexpected error!
+        return -1;
     }
     else if (nread == 0) 
       break;       // EOF
@@ -22,3 +22,25 @@ ssize_t rio_readn(int fd, void *usrbuf, size_t n) {
 
   return n;
 }
+
+ssize_t rio_writen(int fd, void *usrbuf, size_t n) {
+  size_t nleft = n;
+  size_t nwritten;
+  char *bufp = usrbuf;
+
+  while (nleft > 0) {
+    if ((nwritten = write(fd, bufp, nleft)) <= 0) {
+      // Check for interrupt by extraneous sig handler return.
+      if (errno == EINTR)
+        nwritten = 0;
+      else
+        return -1;
+    }
+    nleft -= nwritten;
+    bufp += nwritten;
+  }
+  return n;
+}
+
+
+     
