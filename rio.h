f9d6f297d1b5fd7f78a9338c66502a395a01a791
Willmo3 <thelordofbouge@gmail.com>
2023-10-20 11:52:54 -0400

    Add buffered readers

--- rio.h
+++ rio.h
@@ -5,10 +5,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-/*
- * rio_t: special read buffer that fills itself from fd when empty.
- *
- */
+// rio_t: special read buffer that fills itself from fd when empty.
 #define RIO_BUFSIZE 8192
 typedef struct {
   int rio_fd;        // Descriptor for internal buf
@@ -16,6 +13,20 @@ typedef struct {
   char *rio_bufptr;  // Next unread byte in internal buf
   char rio_buf[RIO_BUFSIZE];
 } rio_t;
+
+// Associates file desc with rb.
+void rio_readinitb(rio_t *rb, int fd);
+
+/*
+ * Reads next text line from rp into usrbuf
+ * Reads at most maxlen -1, appends null term. 
+ *
+ * Returns num bytes read, 0 on EOF, -1 on error.
+ */
+ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen);
+
+// Like readlineb, but instead transfers n raw bytes.
+ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n);
 
 /*
  * Transfers up to n bytes of current file pos of fd
