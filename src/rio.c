c8cc7083a6ad0ad77db583acb578f90de8f89986
Willmo3 <thelordofbouge@gmail.com>
2023-10-20 12:45:30 -0400

    Add rio_read helper

--- src/rio.c
+++ src/rio.c
@@ -47,3 +47,31 @@ void rio_readinitb(rio_t *rp, int fd) {
   rp->rio_cnt = 0;
   rp->rio_bufptr = rp->rio_buf;
 }
+
+// INTERNAL FNS
+
+static ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n) {
+  int cnt;
+
+  while (rp->rio_cnt <= 0) { // Refill if buf empty
+    rp->rio_cnt = read(rp->rio_fd, rp->rio_buf, sizeof(rp->rio_buf));
+
+    if (rp->rio_cnt < 0 && errno != EINTR) {
+      return -1; // Interrupted by sig handler return
+    }
+    else if (rp->rio_cnt == 0) // EOF
+      return 0;
+    else
+      rp->rio_bufptr = rp->rio_buf; // Reset buffer ptr.
+  }
+
+  // Copy min(n, rp->rio_cnt) bytes from internal buf to user buf.
+  cnt = n;
+  if (rp->rio_cnt < n)
+    cnt = rp->rio_cnt;
+  
+  assert(memcpy(usrbuf, rp->rio_bufptr, cnt) != 0); 
+  rp->rio_bufptr += cnt;
+  rp->rio_cnt -= cnt;
+  return cnt;
+}
